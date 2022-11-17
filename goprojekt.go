af1691ecbca5336718a2f93c646e96a3ad218276
Serdar Sadikoglu <serdar.sadikoglu@student.techstarter.de>
2022-11-17 12:07:56 +0100

    switch case erstellen die Wahl haben

--- goprojekt.go
+++ goprojekt.go
@@ -1,7 +1,27 @@
 package main
 
-import ()
+import (
+	"fmt"
+)
 
 func main() {
+
+	i := " "
+
+	fmt.Println("Willkommen im unsere neue GoShell ...")
+	fmt.Println("Bitte geben Sie die gewünchte Aktion ein : ls, mkdir, cd, ls-l")
+	fmt.Scan(&i)
+
+	switch i {
+	case "ls":
+		ls()
+	case "mkdir":
+		Mkdir()
+	case "cd":
+		cd()
+
+	case "ls-l":
+		lsl()
+	}
 
 }
