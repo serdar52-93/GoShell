a6e2a39edd3ef3148018bc6545f90885f1092c97
Serdar Sadikoglu <serdar.sadikoglu@student.techstarter.de>
2022-11-17 11:42:58 +0100

    Erstellen eine Funktion für befehle cd

--- befehle.go
+++ befehle.go
@@ -1,7 +1,19 @@
 package main
 
 import(
-	
+	"fmt"
+	"os"	
 )
+
+// Changing directories/Wechseln von Verzeichnissen (cd)
+func cd() {
+
+	os.Chdir("/Users/serda")
+	newDir, err := os.Getwd()
+	if err != nil {
+	}
+	fmt.Printf("Current Working Directory: %s\n", newDir)
+
+}
 
 
